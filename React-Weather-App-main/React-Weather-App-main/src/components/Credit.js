9a884fa06dec883480e1579f91928bef4c2a74b0
Harshita Srivastava <harshitasrivastava180@gmail.com>
2024-08-25 00:42:45 +0530

    changes

---
+++ React-Weather-App-main/React-Weather-App-main/src/components/Credit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+    Created by Harshita Srivastava
+    </p>
+  </div>
+  );
+}
---
+++ React-Weather-App-main/React-Weather-App-main/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+
+import App from "./components/App";
+import Credit from "./components/Credit";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+    {/* changes */}
+    <Credit />
+  </StrictMode>,
+  rootElement
+);
